ac59110c876770ba92af9aa2079e8b5842ec3125
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-07-05 15:34:54 +0200

    Make similarity function asymmetric

--- utils/similarity.py
+++ utils/similarity.py
@@ -7,8 +7,22 @@ def asymmetrical_kl_sim(mu1: torch.FloatTensor, std1: torch.FloatTensor, mu2: to
     Computes the KL similarity between two normal distributions and returns a tensor with shape (batch_size)
     """
     
+    lower_bound1: torch.FloatTensor = mu1 - (3 * std1)
+    upper_bound1: torch.FloatTensor = mu1 + (3 * std1)
+    
+    lower_bound2: torch.FloatTensor = mu2 - (3 * std2)
+    upper_bound2: torch.FloatTensor = mu2 + (3 * std2)
+
+    is_in: bool = False
+    if lower_bound2 <= lower_bound1 and upper_bound1 <= upper_bound2:
+        is_in = True
+
     p1 = distributions.normal.Normal(mu1, std1)
     p2 = distributions.normal.Normal(mu2, std2)
-    sim = 1 / (1 + distributions.kl.kl_divergence(p1, p2))
 
+    sim = torch.nn.Tanh(distributions.kl.kl_divergence(p1, p2))
+
+    if is_in:
+        return -sim.mean(dim=-1)
+    
     return sim.mean(dim=-1)
